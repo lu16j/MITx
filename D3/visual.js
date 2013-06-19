5ba399d16d9c4c10da805372b1ccca58e3d20e6a
lu16j <lu16j@mit.edu>
2013-06-19 10:53:45 -0400

    D3 svg bar chart

--- D3/visual.js
+++ D3/visual.js
@@ -1,7 +1,13 @@
-var data = [0, 4, 8, 8, 15, 32];
+var data = [4, 77, 8, 32, 0, 40, 8, 15];
 
-var xscale = d3.scale.linear().domain([0, d3.max(data)]).range(["0%","99%"]);
+var chartWidth = 300;
+var chartHeight = 300;
 
-var chart = d3.select(".chart-container").append("div").attr("class", "chart");
+var yscale = d3.scale.linear().domain([0, d3.max(data)]).range([0, chartHeight]);
+var xscale = d3.scale.ordinal().domain(d3.keys(data)).rangeBands([0, chartWidth]);
 
-chart.selectAll("div").data(data).enter().append("div").style("width", xscale).text(function(d) {return d;});
+var chart = d3.select(".chart-container").append("svg").attr("class", "chart").attr("height", chartHeight).attr("width", chartWidth);
+
+chart.selectAll("rect").data(data).enter().append("rect").attr("x", function (d, i) { return xscale(i); }).attr('y', function (d) { return chartHeight - yscale(d); }).attr("width", xscale.rangeBand()).attr("height", yscale);
+
+chart.selectAll("text").data(data).enter().append("text").attr("y", function (d) { return chartHeight - yscale(d) + 5; }).attr("x", function (d, i) { return xscale(i) + xscale.rangeBand()/2; }).attr("dy", "0.7em").attr("text-anchor", "middle").text(function (d) { return d; });
